724bfc4cfcf9b2c783670743f778bda50a2a3064
Zahra-Nfls <Anaflous.Zahra@outlook.com>
2024-09-29 16:58:40 +0200

    created tailwind css workshop

---
+++ React/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header'
+import Form from './components/Form'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Form></Form>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind-laravel/resources/views/footer.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Learning Tailwind</title>
+</head>
+<body>
+    <footer>
+        <div>
+            <p>&copy; 2024 Tailwind Workshop</p>
+        </div>
+    </footer>
+
+
+</body>
+</html>
